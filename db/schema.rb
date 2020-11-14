b47020ae3b9e004ac93476db3f2b22b70422c3f9
Matthew Beal <matt@butcherbox.com>
2020-11-14 10:08:11 -0500

    working on reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003221944) do
+ActiveRecord::Schema.define(version: 20201018162516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guests", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "user_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["reservation_id"], name: "index_guests_on_reservation_id", using: :btree
+    t.index ["user_id"], name: "index_guests_on_user_id", using: :btree
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -91,4 +102,6 @@ ActiveRecord::Schema.define(version: 20171003221944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "guests", "reservations"
+  add_foreign_key "guests", "users"
 end
